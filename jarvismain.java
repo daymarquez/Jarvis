bc44c4ae03a31b181c66314c300d2c17d5af1400
daymarquez <joshua@daymarquez.com>
2016-07-02 12:52:02 -0700

    Update and Rename Program. Add plan + Code
    Changed Name
    Added comments that show what each person does and help me and Kaled communicate better
    Created Shutdown Function...May not currently work.

---
+++ jarvismain.java
@@ -0,0 +1,44 @@
+public class jarvismain {
+
+  public static void main(String[] args) {
+  
+  // Gary's code goes here.
+  // Code should get speech input, analyse it for words, and then
+  	// Call ufncitons that me or kaled make below.
+  
+  }
+
+ public static void turnComputerOff() {
+ 	Runtime runtime = Runtime.getRuntime();
+	Process proc = runtime.exec("shutdown -s -t 0");
+	System.exit(0);
+
+ }
+
+
+// Here are a list of funcitons to implement. For both me and Kaled. 
+ // Take a fucntion from below, move it to the in progress, and then write the code
+ // When done move it to the completed collu.
+
+/*
+Not Started :
+		Turn Volume Off
+		Turn Volume On
+		Raise Volume
+		Lower Volume
+		Check for new emails
+		Set an Alarm
+		Cancel an Alarm
+		Get what the weather is like
+		Just say hello (i.e. Hi there Jarvis! Hello sir!)
+		Start Google Chrome
+		Kill Google Chrome
+		Start Steam
+		Kill Steam
+		Turn on lights. Probably cant do but if we wanna hardware hack this has potential
+		Set a reminder
+		Read out reminders...
+
+		Feel free to add any ideas you get to this project guys!!!!!
+
+}
